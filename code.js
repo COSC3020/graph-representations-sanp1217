2dfdcc5284ea4fea1ef3a3a10dc8f3ed12b8b2ea
pabs <pablosgrad12@gmail.com>
2023-10-18 20:43:48 -0600

    Code for converting adj list to adj matrix

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function convertToAdjMatrix(adjList) {
-    return adjList;
+    let matrix = getEmptyMatrix(adjList.length);
+
+    for (let vertex = 0; vertex < adjList.length; vertex++) {
+        for (let i = 0; i < adjList[vertex].length; i++) {
+            let adjacentVertex = adjList[vertex][i];
+            //Indicate an edge in the matrix 
+            matrix[vertex][adjacentVertex] = 1;
+        }
+    }
+
+    return matrix;
+}
+
+//Returns matrix filled with zeros
+function getEmptyMatrix(size) {
+    let emptMatrix = new Array(size);
+
+    for (let i = 0; i < size; i++) {
+        emptMatrix[i] = new Array(size);
+        for (let j = 0; j < size; j++) {
+            emptMatrix[i][j] = 1;
+        }
+    }
+
 }
